a4be64e7cf51170d70cad6936e67ad34c07d7050
Jaume Diego Monserrat <JDIEMON@upv.edu.es>
2025-10-02 20:32:30 +0200

    Constructor Area

--- ClassLibrary/BusinessLogic/Entities/Area.cs
+++ ClassLibrary/BusinessLogic/Entities/Area.cs
@@ -8,5 +8,16 @@ namespace ManteHos.Entities
 {
     public partial class Area
     {
+        public Area() {
+            //Colecciones
+            Incidents = new List<Incident>();
+        }
+
+        public Area(int id, String name, Master masters)
+        {
+            this.Id =  id;
+            this.Name = name;
+            Master = master;
+        }
     }
 }
