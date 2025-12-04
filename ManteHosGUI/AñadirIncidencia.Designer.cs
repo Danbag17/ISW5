d4fc4a0dd9832602bb1a28df642b274c68df3510
Danbag17 <danibany18@gmail.com>
2025-12-04 19:29:53 +0100

    igu de añadir incidencia

---
+++ ManteHosGUI/AñadirIncidencia.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ManteHosGUI
+{
+    partial class AñadirIncidencia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDepartamento = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(172, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Departamento";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtDepartamento
+            // 
+            this.txtDepartamento.Location = new System.Drawing.Point(282, 31);
+            this.txtDepartamento.Name = "txtDepartamento";
+            this.txtDepartamento.Size = new System.Drawing.Size(100, 22);
+            this.txtDepartamento.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(172, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Descripcion";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(282, 77);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(408, 22);
+            this.txtDescripcion.TabIndex = 3;
+            // 
+            // AñadirIncidencia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDepartamento);
+            this.Controls.Add(this.label1);
+            this.Name = "AñadirIncidencia";
+            this.Text = "AñadirIncidencia";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDepartamento;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDescripcion;
+    }
+}
---
+++ ManteHosGUI/AñadirIncidencia.cs
@@ -0,0 +1,77 @@
+﻿using ManteHos.Entities;
+using ManteHos.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManteHosGUI
+{
+    public partial class AñadirIncidencia : ManteHosFormBase
+    {
+        public AñadirIncidencia()
+        {
+            InitializeComponent();
+        }
+        public AñadirIncidencia(IManteHosService s):base(s)
+        {
+            InitializeComponent();
+        }
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 1. Validaciones básicas
+                if (string.IsNullOrWhiteSpace(txtDepartamento.Text) || string.IsNullOrWhiteSpace(txtDescripcion.Text))
+                {
+                    MessageBox.Show("Rellena todos los campos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // 2. Recoger datos
+                string departamento = txtDepartamento.Text;
+                string descripcion = txtDescripcion.Text;
+                //DateTime fecha = dateFecha.Value;
+
+                // 3. Obtener quién reporta (usuario logueado)
+                Employee reportero = service.UserLogged();
+
+                if (reportero == null)
+                {
+                    MessageBox.Show("No hay usuario logueado. Inicia sesión de nuevo.", "Error");
+                    this.Close();
+                    return;
+                }
+
+                // 4. Crear el objeto
+                //Incident incidente = new Incident(departamento, descripcion, fecha, reportero);
+
+                // 5. Llamar al servicio para guardar en BD
+                //service.AddIncident(incidente);
+
+                MessageBox.Show("Incidencia guardada correctamente.", "Éxito");
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al guardar: " + ex.Message, "Error");
+            }
+        }
+
+        // Evento para el botón Cancelar
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
