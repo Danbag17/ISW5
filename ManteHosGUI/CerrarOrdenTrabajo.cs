c1e410109f26a4374b78734e1d55943e4d0cbd76
MariaR18 <mariaruizr04@gmail.com>
2025-12-12 17:49:34 +0100

    VentanaCerrarOrden

---
+++ ManteHosGUI/CerrarOrdenTrabajo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManteHosGUI
+{
+    public partial class CerrarOrdenTrabajo : Form
+    {
+        public CerrarOrdenTrabajo()
+        {
+            InitializeComponent();
+        }
+
+        private void CerrarOrdenTrabajo_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dfvOrdenes_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void txtInforme_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dtpFechaCierre_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAtras_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCerrarOrden_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
