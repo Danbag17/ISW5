3782a2c3eecc53cf7c483c46a5e05155a77b9587
varos151 <rostivalok21@gmail.com>
2025-12-13 20:56:31 +0100

    confirmación de que se quiere cerrar sesión

--- ManteHosGUI/EmpleadoForm.cs
+++ ManteHosGUI/EmpleadoForm.cs
@@ -30,8 +30,25 @@ namespace ManteHosGUI
 
         private void CerrarSesion_Click(object sender, EventArgs e)
         {
-           this.Close();
+            // 1. Mostramos la ventana emergente preguntando
+            DialogResult respuesta = MessageBox.Show(
+                "¿Estás seguro de que quieres cerrar la sesión?",
+                "Cerrar Sesión",                                
+                MessageBoxButtons.YesNo,                        
+                MessageBoxIcon.Question                          
+            );
 
+            // 2. Comprobamos qué ha pulsado el usuario
+            if (respuesta == DialogResult.Yes)
+            {
+                // SÍ, cerramos la ventana.
+                // Esto hará que el control vuelva al Login (HospitalApp) y se ejecute el Logout real.
+                this.Close();
+            }
+            else
+            {
+                
+            }
         }
 
         private void ReportarIncidencia_Click(object sender, EventArgs e)
